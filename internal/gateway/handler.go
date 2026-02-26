0cf205adce0863485f8c695a86e0a25b15276ff7
Yusuf Yonturk <yusufyonturk.kisisel@gmail.com>
2026-02-26 13:49:09 +0300

    The backend is expected to create the protocol.

--- internal/gateway/handler.go
+++ internal/gateway/handler.go
@@ -1,9 +1,16 @@
 package gateway
 
 import (
+	"context"
 	"encoding/json"
+	"log"
 	"net/http"
+	"time"
+
+	pb "github.com/Chimera-State/GigaScale/internal/pb/reservationv1"
 )
+
+var ReserveClient pb.ReservationServiceClient
 
 func HandleReserve(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -12,11 +19,37 @@ func HandleReserve(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var req ReserveHTTPRequest
+
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		log.printf("JSON Decoding Error %v", err)
+		log.Printf("JSON Decoding Error %v", err)
 		http.Error(w, "Invalid data format", http.StatusBadRequest)
 		return
 	}
+	//mapping
+	grpcReq := &pb.ReserveSeatRequest{
+		UserId:         req.UserID,
+		TripId:         req.TripID,
+		SeatId:         req.SeatID,
+		IdempotencyKey: req.IdempotencyKey,
+	}
+
+	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
+	defer cancel()
+
+	resp, err := ReserveClient.ReserveSeat(ctx, grpcReq)
+	if err != nil {
+		log.Printf("Backend gRPC Error %v", err)
+		http.Error(w, "Backend service was unavailable.", http.StatusInternalServerError)
+		return
+	}
+
+	httpResp := ReserveHTTPResponse{
+		Success: resp.Success,
+		Message: resp.Message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(httpResp)
 
 }
