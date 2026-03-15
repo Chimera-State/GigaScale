1fc3f0ff65fb7517a398f0d42597060669952dc7
Yusuf-Yonturk <yusufyonturk.kisisel@gmail.com>
2026-03-16 01:45:25 +0300

    merge backend gateway

--- internal/gateway/handler.go
+++ internal/gateway/handler.go
@@ -1,42 +1,29 @@
 package gateway
-<<<<<<< HEAD
 
 //handler.go
-=======
->>>>>>> origin/fetaure/sprint3
 import (
 	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 	"time"
+
 	pb "github.com/Chimera-State/GigaScale/api/proto/reservation/v1"
 )
-<<<<<<< HEAD
 
 func (s *Server) HandleReserve(w http.ResponseWriter, r *http.Request) {
 
 	var req ReserveHTTPRequest
 
 	//JSON decode
-=======
-var ReserveClient pb.ReservationServiceClient
-func HandleReserve(w http.ResponseWriter, r *http.Request) {
-	var req ReserveHTTPRequest
-	var validate = validator.New()
->>>>>>> origin/fetaure/sprint3
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		log.Printf("JSON Decoding Error %v", err)
 		http.Error(w, "Invalid data format", http.StatusBadRequest)
 		return
 	}
-<<<<<<< HEAD
 
 	if err := s.validator.Struct(req); err != nil {
-=======
-	if err := validate.Struct(req); err != nil {
->>>>>>> origin/fetaure/sprint3
 		log.Printf("Validation Error: %v", err)
 		http.Error(w, "Data Validation Error: "+err.Error(), http.StatusBadRequest)
 		return
@@ -49,12 +36,8 @@ func HandleReserve(w http.ResponseWriter, r *http.Request) {
 	}
 	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 	defer cancel()
-<<<<<<< HEAD
 
 	resp, err := s.reserveClient.ReserveSeat(ctx, grpcReq)
-=======
-	resp, err := ReserveClient.ReserveSeat(ctx, grpcReq)
->>>>>>> origin/fetaure/sprint3
 	if err != nil {
 		s.handleGRPCError(w, err)
 		return
--- internal/gateway/types.go
+++ internal/gateway/types.go
@@ -1,22 +1,16 @@
 package gateway
-<<<<<<< HEAD:internal/gateway/types.go
 
 //models.go
 
 // istekler için struct
-=======
->>>>>>> origin/fetaure/sprint3:internal/gateway/models.go
 type ReserveHTTPRequest struct {
 	UserID         string `json:"user_id" validate:"required"`
 	TripID         string `json:"trip_id" validate:"required"`
 	SeatID         string `json:"seat_id"  validate:"required,alphanum"`
 	IdempotencyKey string `json:"idempotency_key" validate:"required"`
 }
-<<<<<<< HEAD:internal/gateway/types.go
 
 // yanıtlar için struct
-=======
->>>>>>> origin/fetaure/sprint3:internal/gateway/models.go
 type ReserveHTTPResponse struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
